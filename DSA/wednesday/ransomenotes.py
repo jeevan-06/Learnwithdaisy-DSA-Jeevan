a65a482295374ea009932ec934951930ba84cc36
Jeevankumar M <jeevankumarm06@gmail.com>
2026-08-27 22:56:53 +0530

    Update DSA practices

--- DSA/wednesday/ransomenotes.py
+++ DSA/wednesday/ransomenotes.py
@@ -3,14 +3,12 @@ class Solution:
 
         count = {}
 
-        # Count characters in magazine
         for char in magazine:
             if char in count:
                 count[char] = count[char] + 1
             else:
                 count[char] = 1
-
-        # Check ransomNote characters
+        
         for char in ransomNote:
             if char not in count or count[char] == 0:
                 return False
